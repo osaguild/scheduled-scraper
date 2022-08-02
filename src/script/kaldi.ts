2417e866c38ea7cb34a679680740d3ea56463c60
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-03 07:53:34 +0900

    delete scraping for debug

--- src/script/kaldi.ts
+++ src/script/kaldi.ts
@@ -1,10 +1,14 @@
 import { scraping, selectSales } from "../kaldi/scraping";
 import { sendMessage, createMessage } from "../kaldi/message";
+import { testSales } from "../../test/data";
 
 (async () => {
+  /*
   const sales = await scraping();
   const selectedSales = selectSales(sales, "東京");
   const message = createMessage(selectedSales);
+  */
+  const message = createMessage(testSales);
   const status = await sendMessage(message);
   console.log("status :", status);
 })();

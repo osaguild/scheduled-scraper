ef2dde00f23bb955eaad79121993fb77b912e1c6
osaguild <nobuhisa.abe.0922@gmail.com>
2022-08-06 00:09:52 +0900

    fix file name

--- src/script/kaldi.ts
+++ src/script/kaldi.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 
 (async () => {
   const sales = await scraping();
-  const filePath = `./data/kaldi/sale_info_${getNowDate()}.json`;
+  const filePath = `./data/kaldi/${getNowDate()}.json`;
   fs.writeFile(filePath, JSON.stringify(sales, null, 2), (e) => {
     if (e) {
       console.log("[failed]file is not created.");
